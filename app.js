4bc18e31ccc909579360b018e5b67820a297e5fc
Kevin Chau <kcheez@Kevins-MacBook-Pro.local>
2018-07-20 11:52:24 -0700

    added dynamic read write storage

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 $(document).ready(function() {
   // attach event listener to buttons(input)
   $('.store-btn').on('click', function() {
+    let titleValue = $('.input-field-title').val();
+    let contentValue = $('.input-field-body').val();
 
-    localStorage.setItem('hrext', 'iono about three')
+    localStorage.setItem('titleValue', titleValue);
+    localStorage.setItem('contentValue', contentValue);
   })
   $('.get-btn').on('click', function() {
 
-    console.log(localStorage.getItem('hrext'))
+    let titleValue = localStorage.getItem('titleValue');
+    let contentValue = localStorage.getItem('contentValue');
+
+    $('.debug').html(`<p>${titleValue} ${contentValue}</p>`)
+
+    // console.log(localStorage.getItem('hrext'));
   })
   $('.delete-btn').on('click', function() {
 
-    console.log(localStorage.removeItem('hrext'))
+    console.log(localStorage.removeItem('hrext'));
   })
   // create function stub for read/write/delete
 });
